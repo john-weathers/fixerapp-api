cac545221453ac6161df7f5d1cc4388a9208a0f6
John Weathers <weathers.johnb@gmail.com>
2023-06-20 12:52:25 -0500

    Add router handler for profile update

--- routes/fixers/private.js
+++ routes/fixers/private.js
@@ -4,11 +4,13 @@ const verifyRoles = require('../../middleware/verifyRoles');
 const ROLES = require('../../config/roles');
 const { 
   handleGetProfile,
+  handleUpdateProfile,
 } = require('../../controllers/fixers.controller');
 const router = express.Router();
 
 router.use(verifyFixerJWT, verifyRoles(ROLES.fixer, ROLES.premiumFixer));
 router.get('/profile', handleGetProfile);
+router.patch('/update-profile', handleUpdateProfile);
 
 const workRouter = require('./work');
 router.use('/work', workRouter);
