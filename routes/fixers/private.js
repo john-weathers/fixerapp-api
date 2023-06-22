b17cef04a3ec18ad2ada74c2dbb65649d33465aa
John Weathers <weathers.johnb@gmail.com>
2023-06-22 12:16:20 -0500

    Add settings update route

--- routes/fixers/private.js
+++ routes/fixers/private.js
@@ -5,12 +5,14 @@ const ROLES = require('../../config/roles');
 const { 
   handleGetProfile,
   handleUpdateProfile,
+  handleUpdateSettings,
 } = require('../../controllers/fixers.controller');
 const router = express.Router();
 
 router.use(verifyFixerJWT, verifyRoles(ROLES.fixer, ROLES.premiumFixer));
 router.get('/profile', handleGetProfile);
 router.patch('/update-profile', handleUpdateProfile);
+router.patch('/update-settings', handleUpdateSettings);
 
 const workRouter = require('./work');
 router.use('/work', workRouter);
--- routes/users/private.js
+++ routes/users/private.js
@@ -4,11 +4,15 @@ const verifyRoles = require('../../middleware/verifyRoles');
 const ROLES = require('../../config/roles');
 const { 
   handleGetProfile,
+  handleUpdateProfile,
+  handleUpdateSettings,
 } = require('../../controllers/users.controller');
 const router = express.Router();
 
 router.use(verifyUserJWT, verifyRoles(ROLES.user, ROLES.premiumUser));
 router.get('/profile', handleGetProfile);
+router.patch('/update-profile', handleUpdateProfile);
+router.patch('/update-settings', handleUpdateSettings);
 
 const requestRouter = require('./request');
 router.use('/request', requestRouter);
