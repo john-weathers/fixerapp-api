0d65268754e683a68c8f77bb0e6f27272255b800
John Weathers <weathers.johnb@gmail.com>
2023-07-02 13:59:55 -0500

    Preparation for deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:8000',
-    'http://localhost:8500',
-    'http://127.0.0.1:5173',
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:8500',
+    'https://fixerapp.onrender.com',
 ]
 
 module.exports = allowedOrigins;
